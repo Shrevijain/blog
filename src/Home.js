ef6fa1ac1b415500255e0183e0c7ddc8a54aaf1b
Shrevijain <shrevijainbhl@gmail.com>
2023-04-05 10:18:13 +0530

    committed

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+const Home = () => {
+    const [name, setName] = useState ('Shrevi');
+    const [age, setAge] = useState('19');
+
+    const handleClick = () => {
+        setName ("Utkarsh");
+        setAge ("21");
+    }
+
+    return (
+      <div className="home">
+        <h2>Homepage</h2>
+        <p>{name} is {age} year old</p>
+        <button onClick={handleClick}>click Me</button>
+      </div>
+    );
+  }
+   
+  export default Home;
