1eee615049282bcff2337d478bc4685953fea25b
Shrevijain <shrevijainbhl@gmail.com>
2023-04-13 23:48:17 +0530

    commit for error page

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return ( 
         <div className="not-found">
-            <h2>Sorry</h2>
-            <p>That page is not found</p>
+            <h2>Sorry</h2><br/>
+            <p>That page is not found</p><br></br>
             <Link to="/">Back to the Home page.</Link>
         </div>
      );
